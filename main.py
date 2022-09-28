385e1a3d3ad9f8937da66db3523686e77e7ea04a
jakeFlak <113362895+JakeFlakne10@users.noreply.github.com>
2022-09-28 09:18:07 -0500

    setup

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
 #WRITE YOUR CODE IN THIS FILE
+
+
+def greaterThan():
+    if x  y == true:
+        print("true")
+    else:
+        print("false")
+
+
+def lessThan():
+    if x  y == true:
+        print("true")
+    else:
+        print("false")
+
+        
+def equalTo():
+    if x  y == true:
+        print("true")
+    else:
+        print("false")
+
+
+def greaterOrEqual(x, y):
+    if x  y == true:
+        print("true")
+    else:
+        print("false")
+
+
+def lessOrEqual(x, y):
+    if x ≤ y == True:
+        print("true")
+    else:
+        print("false")
